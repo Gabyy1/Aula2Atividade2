57d69a50649eec59c3b5a7f4b1f1e98f5b181177
Gabrizinha <gabiemanu18@gmail.com>
2022-02-26 18:06:53 -0300

    Primeiro commit

---
+++ src/controller/KillController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import java.io.IOException;
+
+public class KillController {
+  public KillController() {
+	 super(); 
+
+	}
+  public String os() {
+	  String os = System.getProperty("os.name");
+	  return os;
+  }
+   public void KillProcess(String param) {
+	   String cmdPid = "TASKILL /PID";
+	   String cmdNome = "TASKILL /iM";
+	   int pid = 0;
+	   StringBuffer buffer = new StringBuffer();
+	   
+	   try{
+		   pid = Integer.parseInt(param);
+		   buffer.append(cmdPid );
+		   buffer.append(pid);
+	   }
+	   catch(NumberFormatException e) {
+		   buffer.append(cmdNome );
+		   buffer.append(param);
+	   }
+		try {
+			Runtime.getRuntime().exec(buffer.toString());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	   
+   }
+}
---
+++ src/view/Principal.java
@@ -0,0 +1,15 @@
+package view;
+
+import controller.KillController;
+
+public class Principal {
+	 public static void main (String []args) {
+		 KillController Kill = new KillController();
+		 
+		String process = "C:\\Windows\\wfrite.exe";
+		KillController.KillProcess(param);
+	 
+	
+
+}
+}
