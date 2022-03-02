39e06c133157727aee28955f55665a918158edf6
Gabrizinha <gabiemanu18@gmail.com>
2022-03-02 16:08:32 -0300

    Primeiro commit

--- src/controller/KillController.java
+++ src/controller/KillController.java
@@ -1,6 +1,9 @@
 package controller;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class KillController {
   public KillController() {
@@ -10,26 +13,97 @@ public class KillController {
   public String os() {
 	  String os = System.getProperty("os.name");
 	  return os;
+
   }
-   public void KillProcess(String param) {
-	   String cmdPid = "TASKILL /PID";
-	   String cmdNome = "TASKILL /iM";
-	   int pid = 0;
-	   StringBuffer buffer = new StringBuffer();
-	   
-	   try{
-		   pid = Integer.parseInt(param);
-		   buffer.append(cmdPid );
-		   buffer.append(pid);
-	   }
-	   catch(NumberFormatException e) {
-		   buffer.append(cmdNome );
-		   buffer.append(param);
-	   }
+  
+   public void initProcess(String process) {
 		try {
-			Runtime.getRuntime().exec(buffer.toString());
-		} catch (IOException e) {
-			e.printStackTrace();
-		}	   
-   }
-}
+			Runtime.getRuntime().exec(process);
+		} catch (Exception e) {
+			String erro = e.getMessage();
+			if (erro.contains("2")) {
+				System.err.println("Aplicação não encontrada");
+			}
+			if (erro.contains("740")) {
+				StringBuffer buffer = new StringBuffer();
+				buffer.append("cmd /c");
+				buffer.append(" ");
+				buffer.append(process);
+
+				String cmdCred = buffer.toString();
+
+				try {
+					Runtime.getRuntime().exec(cmdCred);
+				} catch (Exception e1) {
+					System.err.println("Chamada de aplicação inválida");
+				}
+			}
+		}
+	}
+  
+  public void readProcess(String process) {
+	  	try {
+	  		Process p = Runtime.getRuntime().exec(process);
+	  		InputStream fluxo = p.getInputStream();
+	  		InputStreamReader leitor = new InputStreamReader(fluxo);
+	  		BufferedReader buffer = new BufferedReader(leitor);
+	  		String linha = buffer.readLine();
+	  		while (linha != null) {
+	  			System.out.println(linha);
+	  			linha = buffer.readLine();
+	  		}
+	  		buffer.close();
+	  		leitor.close();
+	  		fluxo.close();
+	  	} catch (IOException e) {
+	  		System.err.println("Chamada inválida");
+	  		//e.printStackTrace();
+	  	}
+	  	}
+ 
+  public void KillProcess(String param) {
+    	
+    	 String os = System.getProperty("os.name");
+		 String cmdPid = "";
+		 String cmdNome = "";
+		 if (os.contains("Windows")) {
+			// cmdNome = "TASKKILL /IM";
+			 cmdPid = "TASKKILL /PID" + param;
+			 cmdNome = "TASKKILL /IM " + param; 
+			 
+		 }
+		 if (os.contains("Linux")) {
+			 cmdNome = "kill -9";
+			 cmdPid = "pkill -f";
+			 
+		 }
+		   int pid = 0;
+		   
+		   StringBuffer buffer = new StringBuffer();
+		   try{
+			  // pid = Integer.parseInt(param);
+			  // buffer.append(cmdPid );
+			 //  buffer.append(pid);
+			  buffer.append(cmdNome);
+			
+		   }
+		   catch(NumberFormatException e) {
+			  // System.out.println("Erro de formatacao: "+ e.getMessage());
+			   pid = Integer.parseInt(param);
+			   buffer.append(cmdPid );
+			   buffer.append(param);
+		   }
+		   
+		   initProcess(buffer.toString());
+  } 
+   
+      }
+		
+	
+      
+  
+ 
+  
+  
+    
+
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,15 +1,50 @@
 package view;
 
+import javax.swing.JOptionPane;
+
 import controller.KillController;
 
 public class Principal {
 	 public static void main (String []args) {
 		 KillController Kill = new KillController();
+
 		 
-		String process = "C:\\Windows\\wfrite.exe";
-		KillController.KillProcess(param);
+        int option = 0;
+		 do { 
+			 option = Integer.parseInt(JOptionPane.showInputDialog(null, "1 - Mostrar Processos\n" +
+		                                                                 "2 - Matar Processo\n"+
+		                                                                 "3 - Sair"));
+					 
+         	switch(option) {
+         	case 1: 
+         		 String process = "TASKLIST /FO TABLE";
+        		 Kill.readProcess(process);
+        		 break;
+            
+         	case 2: 
+         		String param = "notepad.exe";
+				Kill.KillProcess(param);
+        		break;
+        		
+         	case 3:
+         		JOptionPane.showInputDialog(null,"Encerrado");
+         		break;
+         		
+   }
+         	} while (option != 3);
+        	
+		 }
+	 }
+
+		 
+		// String os = Kill.os();
+		// System.out.println(os);
+		 
+		
+		 
+		
 	 
 	
 
-}
-}
+
+
